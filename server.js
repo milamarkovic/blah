e34574da980822ca77874e6493bc6c68f497f556
Mila <mila.as.markovic@gmail.com>
2017-05-23 21:18:38 +1000

    messages

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
+app.get('/messages', function(req, res){
+  res.send(messages);
+});
+
 var PORT = 3000;
 
 // This is to make sure our server connects on the port Heroku gives it
