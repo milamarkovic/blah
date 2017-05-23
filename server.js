4167b7a966818774bf232830268bb49a0d76a983
Mila <mila.as.markovic@gmail.com>
2017-05-23 21:07:48 +1000

    gave heroku a port

--- server.js
+++ server.js
@@ -10,4 +10,12 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+var PORT = 3000;
+
+// This is to make sure our server connects on the port Heroku gives it
+// When I run this on my own computer though, it will use the one above
+if (process.env.PORT) {
+  PORT = process.env.PORT;
+}
+
+app.listen(PORT);
